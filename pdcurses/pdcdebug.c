0e29734d0edc40525fce0c974edd164bcaabf255
Mark Hessling <mark@rexx.org>
2002-06-23 04:13:02 +0000

    Add timestamp to trace output

--- pdcurses/pdcdebug.c
+++ pdcurses/pdcdebug.c
@@ -17,81 +17,85 @@
 * See the file maintain.er for details of the current maintainer.
 ***************************************************************************
 */
-#define	CURSES_LIBRARY	1
+#define  CURSES_LIBRARY 1
 #ifdef HAVE_CONFIG_H
-#  include <config.h>
+# include <config.h>
 #endif
 #include <curses.h>
 
 #include <string.h>
 #include <sys/types.h>
-#undef	PDC_debug
+#include <time.h>
+
+
+#undef   PDC_debug
 
 #if defined(HAVE_STDARG_H) && defined(HAVE_PROTO)
-#  include <stdarg.h>
-#  define HAVE_STDARG_H_HAVE_PROTO
+# include <stdarg.h>
+# define HAVE_STDARG_H_HAVE_PROTO
 #else
-#  include <varargs.h>
+# include <varargs.h>
 #endif
 
 #ifdef PDCDEBUG
-char *rcsid_PDCdebug  = "$Id: pdcdebug.c,v 1.1 2001/01/10 08:27:20 mark Exp $";
+char *rcsid_PDCdebug  = "$Id: pdcdebug.c,v 1.2 2002/06/23 04:13:02 mark Exp $";
 #endif
 
-	bool trace_on = FALSE;
+   bool trace_on = FALSE;
 
 /*man-start*********************************************************************
 
-  PDC_debug()	- Write debugging info to log file.
+  PDC_debug()  - Write debugging info to log file.
 
   PDCurses Description:
- 	This is a private PDCurses routine.
+   This is a private PDCurses routine.
 
   PDCurses Return Value:
- 	No return value.
+   No return value.
 
   PDCurses Errors:
- 	No errors are defined for this function.
+   No errors are defined for this function.
 
   Portability:
- 	PDCurses	void PDC_debug( char *,... );
+   PDCurses void PDC_debug( char *,... );
 
 **man-end**********************************************************************/
 
 #ifdef HAVE_STDARG_H_HAVE_PROTO
-void	PDC_CDECL	PDC_debug( char *fmt, ... )
+void  PDC_CDECL   PDC_debug( char *fmt, ... )
 #else
-void	PDC_CDECL	PDC_debug(fmt,va_alist)
+void  PDC_CDECL   PDC_debug(fmt,va_alist)
 char *fmt;
 va_dcl
 #endif
 {
-	va_list args;
-FILE *dbfp=NULL;
-char buffer[256];
+   va_list args;
+   FILE *dbfp=NULL;
+   char buffer[256];
 
-/*
- * open debug log file append
-*/
-	buffer[0] = '\0';
-	if (!trace_on)
-		return; 
-	dbfp = fopen("trace","a");
-	if (dbfp == NULL)
-	{
-		fprintf( stderr, "PDC_debug(): Unable to open debug log file\n" );
-		return;
-	}
+   /*
+    * open debug log file append
+    */
+   buffer[0] = '\0';
+   if (!trace_on)
+      return; 
+   dbfp = fopen("trace","a");
+   if (dbfp == NULL)
+   {
+      fprintf( stderr, "PDC_debug(): Unable to open debug log file\n" );
+      return;
+   }
 
 #ifdef HAVE_STDARG_H_HAVE_PROTO
-	va_start(args, fmt);
+   va_start(args, fmt);
 #else
-	va_start(args);
+   va_start(args);
 #endif
 
-	vsprintf(buffer,fmt,args);
-	fputs(buffer,dbfp);
-	va_end(args);
-	fclose(dbfp);
-	return;
+   fprintf( dbfp, "At: %8.8d - %d secs ", clock(), clock() / CLOCKS_PER_SEC );
+   vsprintf(buffer,fmt,args);
+   fputs(buffer,dbfp);
+   va_end(args);
+   fclose(dbfp);
+   return;
 }

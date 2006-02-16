677df73e4c96c0430b7ac8aafffabb859b835385
William McBrine <wmcbrine@gmail.com>
2006-02-16 23:40:30 +0000

    Missed one.

--- x11/pdcx11.h
+++ x11/pdcx11.h
@@ -1,25 +1,9 @@
 /**************************************************************************
-* This file comprises part of PDCurses. PDCurses is Public Domain software.
-* You may use this code for whatever purposes you desire. This software
-* is provided AS IS with NO WARRANTY whatsoever.
-* Should this software be used in another application, an acknowledgement
-* that PDCurses code is used would be appreciated, but is not mandatory.
-*
-* Any changes which you make to this software which may improve or enhance
-* it, should be forwarded to the current maintainer for the benefit of 
-* other users.
-*
-* The only restriction placed on this code is that no distribution of
-* modified PDCurses code be made under the PDCurses name, by anyone
-* other than the current maintainer.
-* 
-* See the file maintain.er for details of the current maintainer.
-*
 * This file is NOT public domain software.  It is Copyright, Mark Hessling
 * 1994-1999. M.Hessling@qut.edu.au
 **************************************************************************/
 
-/* $Id: pdcx11.h,v 1.20 2006/02/05 13:18:10 wmcbrine Exp $ */
+/* $Id: pdcx11.h,v 1.21 2006/02/16 23:40:30 wmcbrine Exp $ */
 
 #define	CURSES_LIBRARY 1
 #ifdef HAVE_CONFIG_H

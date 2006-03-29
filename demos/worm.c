5858457733f18563cc78765d1b7f3fbe149338c9
William McBrine <wmcbrine@gmail.com>
2006-03-29 00:21:50 +0000

    NCurses copyright notices for worm and rain. :-P

--- demos/worm.c
+++ demos/worm.c
@@ -1,3 +1,31 @@
+/****************************************************************************
+ * Copyright (c) 2005 Free Software Foundation, Inc.			    *
+ *									    *
+ * Permission is hereby granted, free of charge, to any person obtaining a  *
+ * copy of this software and associated documentation files (the	    *
+ * "Software"), to deal in the Software without restriction, including	    *
+ * without limitation the rights to use, copy, modify, merge, publish,	    *
+ * distribute, distribute with modifications, sublicense, and/or sell	    *
+ * copies of the Software, and to permit persons to whom the Software is    *
+ * furnished to do so, subject to the following conditions:		    *
+ *									    *
+ * The above copyright notice and this permission notice shall be included  *
+ * in all copies or substantial portions of the Software.		    *
+ *									    *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS  *
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF		    *
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.   *
+ * IN NO EVENT SHALL THE ABOVE COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,   *
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR    *
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR    *
+ * THE USE OR OTHER DEALINGS IN THE SOFTWARE.				    *
+ *									    *
+ * Except as contained in this notice, the name(s) of the above copyright   *
+ * holders shall not be used in advertising or otherwise to promote the     *
+ * sale, use or other dealings in this Software without prior written	    *
+ * authorization.							    *
+ ****************************************************************************/
+
 /*
 
 	 @@@        @@@    @@@@@@@@@@     @@@@@@@@@@@    @@@@@@@@@@@@
@@ -23,7 +51,7 @@ Options:
 	-n <n>			set number of worms
 	-t			make worms leave droppings
 
-  $Id: worm.c,v 1.13 2006/03/01 07:48:33 wmcbrine Exp $
+  $Id: worm.c,v 1.14 2006/03/29 00:21:50 wmcbrine Exp $
 */
 
 #include <curses.h>

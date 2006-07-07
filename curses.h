e2c261a48f1482c9bf15334aa84c3b5bb6abf0a1
William McBrine <wmcbrine@gmail.com>
2006-07-07 03:11:51 +0000

    Updated build number.

--- curses.h
+++ curses.h
@@ -15,7 +15,7 @@
  * See the file maintain.er for details of the current maintainer.	*
  ************************************************************************/
 
-/* $Id: curses.h,v 1.183 2006/07/02 22:03:01 wmcbrine Exp $ */
+/* $Id: curses.h,v 1.184 2006/07/07 03:11:51 wmcbrine Exp $ */
 
 /*----------------------------------------------------------------------*
  *				PDCurses				*
@@ -45,7 +45,7 @@ PDCurses portable platform definitions list:
 
 **man-end****************************************************************/
 
-#define PDC_BUILD 2806
+#define PDC_BUILD 2807
 #define	PDCURSES	1	/* PDCurses-only routines	*/
 #define	XOPEN		1	/* X/Open Curses routines	*/
 #define	SYSVcurses	1	/* System V Curses routines	*/

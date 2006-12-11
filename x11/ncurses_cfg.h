04da172154463b07c5d9859d536c580a19033216
William McBrine <wmcbrine@gmail.com>
2006-12-11 03:04:33 +0000

    Get all tests from ncurses; allow building view in wide mode.

--- x11/ncurses_cfg.h
+++ x11/ncurses_cfg.h
@@ -1,11 +1,11 @@
-/* $Id: ncurses_cfg.h,v 1.4 2006/12/08 02:44:53 wmcbrine Exp $
+/* $Id: ncurses_cfg.h,v 1.5 2006/12/11 03:04:33 wmcbrine Exp $
  *
  * This file is only used with the ncurses test programs.
  *
  * Have ncurses-5.5 unpacked in your $(HOME) (you don't need to build 
  * it), or edit ncurses_testdir appropriately in the Makefile. Configure 
- * and build XCurses. (Sorry, other ports won't work yet.) Change to 
- * this directory, and:
+ * and build PDCurses for X11. (Sorry, other ports won't work yet.) 
+ * Change to this directory, and:
  *
  * "make ncurses_tests" to start.
  * "make ncurses_clean" when you're done.
@@ -18,16 +18,24 @@
 
 #define ExitProgram exit
 
+#define HAVE_GETBEGX 1
+#define HAVE_GETCURX 1
+#define HAVE_GETMAXX 1
 #define HAVE_GETNSTR 1
 #define HAVE_GETTIMEOFDAY 1
-#define HAVE_PANEL_H 1
-#define HAVE_LIBPANEL 1
-#define HAVE_GETCURX 1
-#define HAVE_GETBEGX 1
-#define HAVE_GETMAXX 1
 #define HAVE_GETWIN 1
+#define HAVE_LIBPANEL 1
+#define HAVE_LOCALE_H 1
+#define HAVE_NAPMS 1
+#define HAVE_PANEL_H 1
 #define HAVE_PUTWIN 1
+#define HAVE_SLK_COLOR 1
+#define HAVE_SLK_INIT 1
 #define HAVE_WRESIZE 1
+
+#ifdef PDC_WIDE
+# define USE_WIDEC_SUPPORT 1
+#endif
 
 /* Fool ncurses.c so it gives us all the tests, and doesn't redefine 
    ACS_ chars

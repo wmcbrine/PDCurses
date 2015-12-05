0bd221972ead5338c99ac2655ed0810a7422de27
William McBrine <wmcbrine@gmail.com>
2015-12-05 12:31:15 -0500

    Stricter C. Problem reported by Damien Gaignon.

--- pdcurses/clear.c
+++ pdcurses/clear.c
@@ -84,13 +84,15 @@ int clrtoeol(void)
 
 int wclrtobot(WINDOW *win)
 {
+    int savey, savex;
+
     PDC_LOG(("wclrtobot() - called\n"));
 
     if (!win)
         return ERR;
 
-    int savey = win->_cury;
-    int savex = win->_curx;
+    savey = win->_cury;
+    savex = win->_curx;
 
     /* should this involve scrolling region somehow ? */
 

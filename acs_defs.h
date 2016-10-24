5f89a14aabd69f0e1ed7277a7cb487702cd503ee
Bill-Gray <pluto@projectpluto.com>
2016-10-24 12:54:06 -0400

    Moved acs_map[] from various pdcdisp.c files into a common acs_defs.h file.  The assorted versions were almost entirely identical;  combining them was straightforward and should make maintenance/understanding the code easier.  Also added overlined,  dimmed,  and strikeout text in 32-bit chtype builds with 8-bit charsets.

--- acs_defs.h
+++ acs_defs.h
@@ -1,12 +1,13 @@
-/* Most of the following #defines are completely unused for the
-nonce.  They allow one to use either code page 437 renditions of
-the alternative character set (ACS) or the Unicode versions.
-(i.e.,  uppercase C with a cedilla is 0x80 for code page 437,
-but is 0xc7 in Unicode.)
-   Traditionally,  there are 32 ACS characters,  so that only
-32 of the following would actually be used.  However,  we _could_
-have up to 128 ACS characters,  in which case we'd be using most
-of the following.               */
+/* Many of the following #defines are completely unused for the
+nonce.  For each character,  its code point in code page 437,
+Unicode,  and page 8859-1 are given.  The first is used for
+non-wide builds in Win32 console,  DOS,  SDL,  and OS/2.
+Unicode is used for all wide builds,  and for the non-wide
+build of Win32a.  Code page 8859-1 is used for non-wide X11.
+
+   All of these characters exist in CP437 and Unicode.  Some
+don't exist in 8859-1,  in which case the last column is 'TBD'.
+Only 32 are used in ncurses.  So caution is advised. */
 
 #ifdef USE_ISO8859_CHARSET
    #define CHOOSE( A, B, C)   (C)
@@ -16,6 +17,38 @@ of the following.               */
 #endif
 
 /* Codes found from https://en.wikipedia.org/wiki/Code_page_437 */
+
+#define SMILE                             CHOOSE( 0x01,  0x263a,  'O')
+#define REV_SMILE                         CHOOSE( 0x02,  0x263b,  'O')
+#define HEART                             CHOOSE( 0x03,  0x2665,  'H')
+#define DIAMOND                           CHOOSE( 0x04,  0x2666, 0x01)
+#define CLUB                              CHOOSE( 0x05,  0x2663,  'C')
+#define SPADE                             CHOOSE( 0x06,  0x2660,  'S')
+#define MEDIUM_BULLET                     CHOOSE( 0x07,  0x2022, 0xb7)
+#define REV_BULLET                        CHOOSE( 0x08,  0x2508, 0xb7)
+#define WHITE_BULLET                      CHOOSE( 0x09,  0x25cb,    7)
+#define REV_WHITE_BULLET                  CHOOSE( 0x0a,  0x25D9,    7)
+#define MALE_SYM                          CHOOSE( 0x0b,  0x2642,  'm')
+#define FEMALE_SYM                        CHOOSE( 0x0c,  0x2640,  'f')
+#define QTR_NOTE                          CHOOSE( 0x0d,  0x266a, 0xbc)
+#define EIGHTH_NOTE                       CHOOSE( 0x0e,  0x266b, 0xbd)
+#define SPLAT                             CHOOSE( 0x0f,  0xa4  , 0xa4)
+#define RIGHT_TRIANGLE                    CHOOSE( 0x10,  0x25b6,  '>')
+#define LEFT_TRIANGLE                     CHOOSE( 0x11,  0x25c0,  '<')
+#define UP_DOWN_ARROW                     CHOOSE( 0x12,  0x2195, 0x19)
+#define DBL_BANG                          CHOOSE( 0x13,  0x203c,  '!')
+#define PILCROW                           CHOOSE( 0x14,  0xb6  , 0xb6)
+#define SECTION_SIGN                      CHOOSE( 0x15,  0xa7  , 0xa7)
+#define LOW_QTR_BLOCK                     CHOOSE( 0x16,  0x25b2,  '_')
+#define UP_DOWN_ARROW_UNDERSCORED         CHOOSE( 0x17,  0x21ab, 0x19)
+#define UP_ARROW                          CHOOSE( 0x18,  0x2191,  '^')
+#define DOWN_ARROW                        CHOOSE( 0x19,  0x2193,  'v')
+#define RIGHT_ARROW                       CHOOSE( 0x1a,  0x2192,  '>')
+#define LEFT_ARROW                        CHOOSE( 0x1b,  0x2190,  '<')
+#define RIGHT_ANGLE                       CHOOSE( 0x1c,  0x221f,  0xe)
+#define LEFT_RIGHT_ARROW                  CHOOSE( 0x1d,  0x2194,  '-')
+#define UP_TRIANGLE                       CHOOSE( 0x1e,  0x25b2,  '^')
+#define DOWN_TRIANGLE                     CHOOSE( 0x1f,  0x25bc,  'v')
 
 #define UPPERCASE_C_CEDILLA               CHOOSE( 0x80,  0xc7  , 0xc7)
 #define LOWERCASE_U_UMLAUT                CHOOSE( 0x81,  0xfc  , 0xfc)
@@ -71,43 +104,48 @@ of the following.               */
 #define LIGHT_SHADE                       CHOOSE( 0xb0,  0x2591, '#' )
 #define MEDIUM_SHADE                      CHOOSE( 0xb1,  0x2592,    2)
 #define DARK_SHADE                        CHOOSE( 0xb2,  0x2593,  TBD)
-#define VERTICAL                          CHOOSE( 0xb3,  0x2502,   25)
-#define VERTICAL_LEFT                     CHOOSE( 0xb4,  0x2524,   22)
-#define VERTICAL_DOUBLE_LEFT_SINGLE       CHOOSE( 0xb5,  0x2561,  TBD)
-#define VERTICAL_SINGLE_LEFT_DOUBLE       CHOOSE( 0xb6,  0x2562,  TBD)
-#define DOWN_DOUBLE_LEFT_SINGLE           CHOOSE( 0xb7,  0x2556,  TBD)
-#define LEFT_DOUBLE_DOWN_SINGLE           CHOOSE( 0xb8,  0x2555,  TBD)
-#define DOWN_DOUBLE_LEFT_DOUBLE           CHOOSE( 0xbb,  0x2557,  TBD)
-#define UP_DOUBLE_LEFT_SINGLE             CHOOSE( 0xbd,  0x255c,  TBD)
-#define UP_SINGLE_LEFT_DOUBLE             CHOOSE( 0xbe,  0x255b,  TBD)
-#define DOWN_LEFT                         CHOOSE( 0xbf,  0x2510,   12)
+#define BOX_VLINE                         CHOOSE( 0xb3,  0x2502,   25)
+#define BOX_RTEE                          CHOOSE( 0xb4,  0x2524,   22)
+#define BOX_SD_RTEE                       CHOOSE( 0xb5,  0x2561,   22)
+#define BOX_DS_RTEE                       CHOOSE( 0xb6,  0x2562,   22)
+#define BOX_DS_URCORNER                   CHOOSE( 0xb7,  0x2556,   12)
+#define BOX_SD_URCORNER                   CHOOSE( 0xb8,  0x2555,   12)
+#define BOX_D_RTEE                        CHOOSE( 0xb9,  0x2563,   22)
+#define BOX_D_VLINE                       CHOOSE( 0xba,  0x2551,   25)
+#define BOX_D_URCORNER                    CHOOSE( 0xbb,  0x2557,   12)
+#define BOX_D_LRCORNER                    CHOOSE( 0xbc,  0x255D,   11)
+#define BOX_DS_LRCORNER                   CHOOSE( 0xbd,  0x255c,   11)
+#define BOX_SD_LRCORNER                   CHOOSE( 0xbe,  0x255b,   11)
+#define BOX_URCORNER                      CHOOSE( 0xbf,  0x2510,   12)
 
-#define UP_RIGHT                          CHOOSE( 0xc0,  0x2514,   14)
-#define UP_HORIZONTAL                     CHOOSE( 0xc1,  0x2534,   23)
-#define DOWN_HORIZONTAL                   CHOOSE( 0xc2,  0x252c,   24)
-#define RIGHT_VERTICAL                    CHOOSE( 0xc3,  0x251c,   21)
-#define HORIZONTAL                        CHOOSE( 0xc4,  0x2500,   18)
-#define HORIZONTAL_VERTICAL               CHOOSE( 0xc5,  0x253c,   15)
-#define VERTICAL_SINGLE_RIGHT_DOUBLE      CHOOSE( 0xc6,  0x255e,  TBD)
-#define VERTICAL_DOUBLE_RIGHT_SINGLE      CHOOSE( 0xc7,  0x255f,  TBD)
-#define DOWN_DOUBLE_RIGHT_DOUBLE          CHOOSE( 0xc9,  0x2554,  TBD)
-#define DOWN_DOUBLE_HORIZONTAL_DOUBLE     CHOOSE( 0xcb,  0x2566,  TBD)
-#define VERTICAL_DOUBLE_RIGHT_DOUBLE      CHOOSE( 0xcc,  0x2560,  TBD)
-#define HORIZONTAL_DOUBLE                 CHOOSE( 0xcd,  0x2560,  TBD)
-#define HORIZONTAL_DOUBLE_VERTICAL_DOUBLE CHOOSE( 0xce,  0x256c,  TBD)
-#define HORIZONTAL_DOUBLE_UP_SINGLE       CHOOSE( 0xcf,  0x2567,  TBD)
+#define BOX_LLCORNER                      CHOOSE( 0xc0,  0x2514,   14)
+#define BOX_BTEE                          CHOOSE( 0xc1,  0x2534,   23)
+#define BOX_TTEE                          CHOOSE( 0xc2,  0x252c,   24)
+#define BOX_LTEE                          CHOOSE( 0xc3,  0x251c,   21)
+#define BOX_HLINE                         CHOOSE( 0xc4,  0x2500,   18)
+#define BOX_PLUS                          CHOOSE( 0xc5,  0x253c,   15)
+#define BOX_SD_LTEE                       CHOOSE( 0xc6,  0x255e,   21)
+#define BOX_DS_LTEE                       CHOOSE( 0xc7,  0x255f,   21)
+#define BOX_D_LLCORNER                    CHOOSE( 0xc8,  0x255A,   14)
+#define BOX_D_ULCORNER                    CHOOSE( 0xc9,  0x2554,   13)
+#define BOX_D_BTEE                        CHOOSE( 0xca,  0x2569,   23)
+#define BOX_D_TTEE                        CHOOSE( 0xcb,  0x2566,   24)
+#define BOX_D_LTEE                        CHOOSE( 0xcc,  0x2560,   21)
+#define BOX_D_HLINE                       CHOOSE( 0xcd,  0x2550,   18)
+#define BOX_D_PLUS                        CHOOSE( 0xce,  0x256C,   15)
+#define BOX_SD_BTEE                       CHOOSE( 0xcf,  0x2567,   23)
 
-#define HORIZONTAL_SINGLE_UP_DOUBLE       CHOOSE( 0xd0,  0x2568,  TBD)
-#define HORIZONTAL_DOUBLE_DOWN_SINGLE     CHOOSE( 0xd1,  0x2564,  TBD)
-#define HORIZONTAL_SINGLE_DOWN_DOUBLE     CHOOSE( 0xd2,  0x2565,  TBD)
-#define RIGHT_SINGLE_UP_DOUBLE            CHOOSE( 0xd3,  0x2559,  TBD)
-#define RIGHT_DOUBLE_UP_SINGLE            CHOOSE( 0xd4,  0x2558,  TBD)
-#define RIGHT_DOUBLE_DOWN_SINGLE          CHOOSE( 0xd5,  0x2552,  TBD)
-#define RIGHT_SINGLE_DOWN_DOUBLE          CHOOSE( 0xd6,  0x2553,  TBD)
-#define HORIZONTAL_SINGLE_VERTICAL_DOUBLE CHOOSE( 0xd7,  0x256b,  TBD)
-#define HORIZONTAL_DOUBLE_VERTICAL_SINGLE CHOOSE( 0xd8,  0x256a,  TBD)
-#define LEFT_UP                           CHOOSE( 0xd9,  0x2518,   11)
-#define RIGHT_DOWN                        CHOOSE( 0xda,  0x250c,   13)
+#define BOX_DS_BTEE                       CHOOSE( 0xd0,  0x2568,   23)
+#define BOX_SD_TTEE                       CHOOSE( 0xd1,  0x2564,   24)
+#define BOX_DS_TTEE                       CHOOSE( 0xd2,  0x2565,   24)
+#define BOX_DS_LLCORNER                   CHOOSE( 0xd3,  0x2559,   14)
+#define BOX_SD_LLCORNER                   CHOOSE( 0xd4,  0x2558,   14)
+#define BOX_SD_ULCORNER                   CHOOSE( 0xd5,  0x2552,   13)
+#define BOX_DS_ULCORNER                   CHOOSE( 0xd6,  0x2553,   13)
+#define BOX_DS_PLUS                       CHOOSE( 0xd7,  0x256b,   15)
+#define BOX_SD_PLUS                       CHOOSE( 0xd8,  0x256a,   15)
+#define BOX_LRCORNER                      CHOOSE( 0xd9,  0x2518,   11)
+#define BOX_ULCORNER                      CHOOSE( 0xda,  0x250c,   13)
 #define FULL_BLOCK                        CHOOSE( 0xdb,  0x2588,    0)
 #define LOWER_HALF_BLOCK                  CHOOSE( 0xdc,  0x2584,  TBD)
 #define LEFT_HALF_BLOCK                   CHOOSE( 0xdd,  0x258c,  TBD)
@@ -139,8 +177,8 @@ of the following.               */
 #define LOWER_HALF_INTEGRAL_SIGN          CHOOSE( 0xf5,  0x2321,  TBD)
 #define DIVISION_SIGN                     CHOOSE( 0xf6,  0x00f7, 0xf7)
 #define APPROXIMATELY_EQUALS_SIGN         CHOOSE( 0xf7,  0x2248,  TBD)
-#define DEGREE_SIGN                       CHOOSE( 0xf8,  0x00b0,    7)
-#define LARGE_BULLET                      CHOOSE( 0xf9,  0x2219,  TBD)
+#define DEGREE_SIGN                       CHOOSE( 0xf8,  0x00b0, 0xb0)
+#define LARGE_BULLET                      CHOOSE( 0xf9,  0x2219,    7)
 #define SMALL_BULLET                      CHOOSE( 0xfa,  0x00b7, 0xb7)
 #define SQUARE_ROOT                       CHOOSE( 0xfb,  0x221a,  TBD)
 #define SUPERSCRIPT_N                     CHOOSE( 0xfc,  0x207f,  TBD)
@@ -148,24 +186,6 @@ of the following.               */
 #define CENTERED_SQUARE                   CHOOSE( 0xfe,  0x25a0,  TBD)
 #define NON_BREAKING_SPACE                CHOOSE( 0xff,  0x00a0,  TBD)
 
-#define BOX_DBL_TEE_LEFT                  CHOOSE( 0xb9,  0x2563,   22)
-#define BOX_DBL_VERTICAL                  CHOOSE( 0xba,  0x2551,   19)
-#define BOX_DBL_DOWN_LEFT                 CHOOSE( 0xbb,  0x2557,   12)
-#define BOX_DBL_UP_LEFT                   CHOOSE( 0xbc,  0x255D,   11)
-#define BOX_DBL_UP_RIGHT                  CHOOSE( 0xc8,  0x255A,   14)
-#define BOX_DBL_DOWN_RIGHT                CHOOSE( 0xc9,  0x2554,   13)
-#define BOX_DBL_TEE_UP                    CHOOSE( 0xca,  0x2569,   17)
-#define BOX_DBL_TEE_DOWN                  CHOOSE( 0xcb,  0x2566,   18)
-#define BOX_DBL_TEE_RIGHT                 CHOOSE( 0xcc,  0x2560,   21)
-#define BOX_DBL_HORIZONTAL                CHOOSE( 0xcd,  0x2550,   18)
-#define BOX_DBL_CROSS                     CHOOSE( 0xce,  0x256C,   15)
-
-#define RIGHT_ARROW                       CHOOSE( 0x1a,  0x2192,  '>')
-#define LEFT_ARROW                        CHOOSE( 0x1b,  0x2190,  '<')
-#define UP_ARROW                          CHOOSE( 0x18,  0x2191,  '^')
-#define DOWN_ARROW                        CHOOSE( 0x19,  0x2193,  'v')
-#define DIAMOND                           CHOOSE( 0x04,  0x2666, 0x01)
-#define SPLAT                             CHOOSE( 0x0f,  0xa4  , 0xa4)
 
 
       /* It says at http://unicode.org/charts/PDF/U2300.pdf */
@@ -180,9 +200,66 @@ of the following.               */
 #define HORIZ_SCAN_LINE_1                 CHOOSE( 0x2d,  0x23ba,   16)
 #define HORIZ_SCAN_LINE_3                 CHOOSE( 0x2d,  0x23bb,   17)
 #define HORIZ_SCAN_LINE_7                 CHOOSE( 0x2d,  0x23bc,   19)
-#define HORIZ_SCAN_LINE_9                 CHOOSE( 0x2d,  0x23bd,   20)
+#define HORIZ_SCAN_LINE_9                 CHOOSE(  '_',  0x23bd,   20)
 
       /* Code page 437 lacks a 'for real' not-equals,  so for that, */
       /* we use the double-horizontal single-vertical box drawing : */
 #define NOT_EQUALS_SIGN                   CHOOSE( 0xd8, 0x2260,    29)
 
+# define A(x) ((chtype)x | A_ALTCHARSET)
+
+chtype acs_map[128] =
+{
+   A(0), A(1), A(2), A(3), A(4), A(5), A(6), A(7), A(8),
+   A(9), A(10),
+   CLUB, HEART, SPADE, SMILE, REV_SMILE,                      /* 11 12 13 14 15 */
+   MEDIUM_BULLET, WHITE_BULLET, PILCROW, SECTION_SIGN,        /* 16 17 18 19 */
+   A_ORDINAL, O_ORDINAL, LOWERCASE_PHI,                       /* 20 21 22 */
+   INVERTED_EXCLAMATION_MARK, INVERTED_QUESTION_MARK,         /* 23 24 */
+   REVERSED_NOT_SIGN, NOT_SIGN,                               /* 25 26 */
+   UPPER_HALF_INTEGRAL_SIGN, LOWER_HALF_INTEGRAL_SIGN,        /* 27 28   */
+   SUPERSCRIPT_N, CENTERED_SQUARE, F_WITH_HOOK,               /* 29 30 31 */
+
+   RIGHT_ARROW, LEFT_ARROW, UP_ARROW, DOWN_ARROW,             /* 32 !"# */
+
+   PI, NOT_EQUALS_SIGN,  VULGAR_HALF, VULGAR_QUARTER,         /* $%&' */
+   '(',
+   LEFT_ANGLE_QUOTE_MARK, RIGHT_ANGLE_QUOTE_MARK,             /* )* */
+   DARK_SHADE, SUPERSCRIPT_2, INFINITY,                       /* +,- */
+   ALPHA, BETA, GAMMA, UPPERCASE_SIGMA, LOWERCASE_SIGMA,      /* ./012  */
+   '3',
+   MU, TAU, UPPERCASE_PHI, THETA, OMEGA, DELTA, EPSILON,      /* 456789: */
+
+   BOX_SD_LRCORNER, BOX_SD_URCORNER, BOX_SD_ULCORNER,         /* ;<= */
+   BOX_SD_LLCORNER, BOX_SD_PLUS,                              /* >? */
+   BOX_SD_LTEE, BOX_SD_RTEE, BOX_SD_BTEE, BOX_SD_TTEE,        /* @ABC */
+
+   BOX_D_LRCORNER, BOX_D_URCORNER, BOX_D_ULCORNER,             /* DEF */
+   BOX_D_LLCORNER, BOX_D_PLUS,                                 /* GH */
+   BOX_D_LTEE, BOX_D_RTEE, BOX_D_BTEE, BOX_D_TTEE,             /* IJKL */
+
+   BOX_DS_LRCORNER, BOX_DS_URCORNER, BOX_DS_ULCORNER,          /* MNO */
+   BOX_DS_LLCORNER, BOX_DS_PLUS,                               /* PQ */
+   BOX_DS_LTEE, BOX_DS_RTEE, BOX_DS_BTEE, BOX_DS_TTEE,         /* RSTU */
+
+   BOX_LRCORNER, BOX_URCORNER, BOX_ULCORNER,                   /* VWX */
+   BOX_LLCORNER, BOX_PLUS,                                     /* YZ */
+   BOX_LTEE, BOX_RTEE, BOX_BTEE, BOX_TTEE,                     /* [\]^ */
+
+   BOX_HLINE, BOX_VLINE, BOX_D_HLINE, BOX_D_VLINE,             /* _`ab */
+
+   DIVISION_SIGN, APPROXIMATELY_EQUALS_SIGN,                   /* cd */
+   INTERSECTION, TRIPLE_BAR,                                   /* ef */
+   SMALL_BULLET, LARGE_BULLET, SQUARE_ROOT,                    /* ghi */
+   DIAMOND, MEDIUM_SHADE,                                      /* jk */
+   HORIZ_SCAN_LINE_1, HORIZ_SCAN_LINE_3,                       /* lm */
+   HORIZ_SCAN_LINE_7, HORIZ_SCAN_LINE_9,                       /* no */
+   UPPER_HALF_BLOCK, LOWER_HALF_BLOCK,                         /* pq */
+   LEFT_HALF_BLOCK, RIGHT_HALF_BLOCK, FULL_BLOCK,              /* rst */
+   LESSER_THAN_OR_EQUAL_TO, GREATER_THAN_OR_EQUAL_TO,          /* uv */
+   DEGREE_SIGN, PLUS_OR_MINUS, LIGHT_SHADE, SPLAT,             /* wxyz */
+   CENT_SIGN, YEN_SIGN, PESETA_SIGN, STERLING_SIGN,            /* {|}~ */
+    A(127)
+};
+
+# undef A
--- x11/pdcdisp.c
+++ x11/pdcdisp.c
@@ -6,8 +6,6 @@
 
 #ifdef CHTYPE_LONG
 
-# define A(x) ((chtype)x | A_ALTCHARSET)
-
 #ifdef PDC_WIDE
    #define USE_UNICODE_ACS_CHARS 1
 #else
@@ -15,64 +13,6 @@
 #endif
 
 #include "acs_defs.h"
-
-chtype acs_map[128] =
-{
-   A_ORDINAL, O_ORDINAL, INVERTED_QUESTION_MARK,                /* 0 1 2 */
-   REVERSED_NOT_SIGN, NOT_SIGN, VULGAR_HALF,                    /* 3 4 5 */
-   VULGAR_QUARTER, INVERTED_EXCLAMATION_MARK,                   /* 6 7   */
-   LEFT_ANGLE_QUOTE_MARK, RIGHT_ANGLE_QUOTE_MARK,               /* 8 9   */
-   UPPER_HALF_INTEGRAL_SIGN, LOWER_HALF_INTEGRAL_SIGN,          /* 10 11 */
-   SUPERSCRIPT_N, CENTERED_SQUARE, PESETA_SIGN, F_WITH_HOOK,    /* 12-15 */
-   RIGHT_SINGLE_DOWN_DOUBLE, RIGHT_SINGLE_UP_DOUBLE,            /* 16 17 */
-   DOWN_DOUBLE_LEFT_SINGLE, UP_DOUBLE_LEFT_SINGLE,              /* 18 19 */
-   VERTICAL_DOUBLE_RIGHT_SINGLE, VERTICAL_DOUBLE_LEFT_SINGLE,   /* 20 21 */
-   HORIZONTAL_SINGLE_DOWN_DOUBLE, HORIZONTAL_SINGLE_UP_DOUBLE,  /* 22 23 */
-   HORIZONTAL_SINGLE_VERTICAL_DOUBLE,                           /* 24    */
-   RIGHT_DOUBLE_DOWN_SINGLE, RIGHT_DOUBLE_UP_SINGLE,            /* 25 26 */
-   LEFT_DOUBLE_DOWN_SINGLE, UP_SINGLE_LEFT_DOUBLE,              /* 27 28 */
-   VERTICAL_SINGLE_RIGHT_DOUBLE, VERTICAL_SINGLE_LEFT_DOUBLE,   /* 29 30 */
-   HORIZONTAL_DOUBLE_DOWN_SINGLE, HORIZONTAL_DOUBLE_UP_SINGLE,  /* 31 ' ' */
-   HORIZONTAL_DOUBLE_VERTICAL_SINGLE,                           /* !     */
-
-    '"', '#', '$', '%', '&', '\'', '(',
-    ')', '*',
-
-   RIGHT_ARROW, LEFT_ARROW, UP_ARROW, DOWN_ARROW,                   /* +,-.  */
-   '/', FULL_BLOCK,                                                 /* /0    */
-   CENT_SIGN, YEN_SIGN, PESETA_SIGN, VULGAR_HALF, VULGAR_QUARTER,   /* 12345 */
-   LEFT_ANGLE_QUOTE_MARK, RIGHT_ANGLE_QUOTE_MARK, DARK_SHADE,       /* 678 */
-   BOX_DBL_DOWN_RIGHT, BOX_DBL_UP_RIGHT,                            /* 9: */
-   BOX_DBL_DOWN_LEFT, BOX_DBL_UP_LEFT,                              /* ;< */
-   BOX_DBL_TEE_LEFT, BOX_DBL_TEE_RIGHT,                             /* => */
-   BOX_DBL_TEE_UP, BOX_DBL_TEE_DOWN,                                /* ?@ */
-   BOX_DBL_HORIZONTAL, BOX_DBL_VERTICAL,                            /* AB */
-   BOX_DBL_CROSS, SUPERSCRIPT_2,                                    /* CD */
-   ALPHA, BETA, GAMMA, UPPERCASE_SIGMA, LOWERCASE_SIGMA,         /* EFGHI */
-   MU, TAU, UPPERCASE_PHI, THETA, OMEGA, DELTA, INFINITY,       /* JKLMNOP */
-   LOWERCASE_PHI, EPSILON, INTERSECTION, TRIPLE_BAR,            /* QRST */
-   DIVISION_SIGN, APPROXIMATELY_EQUALS_SIGN, SMALL_BULLET,      /* UVW */
-   SQUARE_ROOT, UPPER_HALF_BLOCK, LOWER_HALF_BLOCK,             /* XYZ */
-   LEFT_HALF_BLOCK, RIGHT_HALF_BLOCK,                           /* [\ */
-
-   ']', '^', '_',
-    DIAMOND, MEDIUM_SHADE,          /* `a */
-
-    'b', 'c', 'd', 'e',
-
-   DEGREE_SIGN, PLUS_OR_MINUS, LIGHT_SHADE, SPLAT,          /* fghi */
-   LEFT_UP, DOWN_LEFT, RIGHT_DOWN, UP_RIGHT,                /* jklm */
-   HORIZONTAL_VERTICAL, HORIZ_SCAN_LINE_1, HORIZ_SCAN_LINE_3,  /* nop */
-   HORIZONTAL, HORIZ_SCAN_LINE_7, HORIZ_SCAN_LINE_9,        /* qrs */
-   RIGHT_VERTICAL, VERTICAL_LEFT, UP_HORIZONTAL,            /* tuv */
-   DOWN_HORIZONTAL, VERTICAL, LESSER_THAN_OR_EQUAL_TO,      /* wxy */
-   GREATER_THAN_OR_EQUAL_TO, PI, NOT_EQUALS_SIGN,           /* z{| */
-   STERLING_SIGN, LARGE_BULLET,                             /* }~  */
-
-    A(127)
-};
-
-# undef A
 
 #endif
 

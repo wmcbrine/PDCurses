b98c1d14233296c99bcd4a1f13e40595aaf098ba
Bill-Gray <pluto@projectpluto.com>
2016-11-22 15:29:17 -0500

    MinGWin defines struct _CONSOLE_SCREEN_BUFFER_INFOEX;  to avoid a duplication,  #define HAVE_INFOEX,  just as with Cygwin, OpenWATCOM, and MSVC

--- win32/pdcwin.h
+++ win32/pdcwin.h
@@ -17,8 +17,7 @@
     # define PDC_ATTR_SHIFT  8
 #endif
 #endif
-
-#if (defined(__CYGWIN32__) || defined(__WATCOMC__) || defined(_MSC_VER)) && \
+#if (defined(__CYGWIN32__) || defined(__MINGW32__) || defined(__WATCOMC__) || defined(_MSC_VER)) && \
     !defined(HAVE_INFOEX)
 # define HAVE_INFOEX
 #endif

e2e9962a137e571e7b5114bb44bfe3aefd9f7544
William McBrine <wmcbrine@gmail.com>
2017-03-27 18:12:06 -0400

    Spacing.

--- curses.h
+++ curses.h
@@ -214,10 +214,10 @@ typedef unsigned long mmask_t;
 
 typedef struct
 {
-        short id;       /* unused, always 0 */
-        int x, y, z;    /* x, y same as MOUSE_STATUS; z unused */
-        mmask_t bstate; /* equivalent to changes + button[], but
-                           in the same format as used for mousemask() */
+    short id;       /* unused, always 0 */
+    int x, y, z;    /* x, y same as MOUSE_STATUS; z unused */
+    mmask_t bstate; /* equivalent to changes + button[], but
+                       in the same format as used for mousemask() */
 } MEVENT;
 
 #ifdef NCURSES_MOUSE_VERSION

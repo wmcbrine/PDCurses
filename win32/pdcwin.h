8e172448ff74d38d9207f7c1f289b175fef96051
Bill-Gray <pluto@projectpluto.com>
2017-05-07 16:25:38 -0400

    Simon Sobisch pointed out that there is no such thing as __CYGWIN32__;  it should be just __CYGWIN__

--- win32/pdcwin.h
+++ win32/pdcwin.h
@@ -17,7 +17,7 @@
     # define PDC_ATTR_SHIFT  8
 #endif
 #endif
-#if (defined(__CYGWIN32__) || defined(__MINGW32__) || defined(__WATCOMC__) \
+#if (defined(__CYGWIN__) || defined(__MINGW32__) || defined(__WATCOMC__) \
      || (defined(_MSC_VER) && _MSC_VER >= 1900))
    #if !defined(HAVE_INFOEX)
        # define HAVE_INFOEX

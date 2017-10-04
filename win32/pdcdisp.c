64144679f8a5842f2d3923bae461c45463185936
Bill-Gray <pluto@projectpluto.com>
2017-10-03 20:07:18 -0400

    Display of fullwidth characters (in consoles supporting that display)

--- win32/pdcdisp.c
+++ win32/pdcdisp.c
@@ -60,26 +60,35 @@ void PDC_transform_line(int lineno, int x, int len, const chtype *srcp)
     sr.Right = x + len - 1;
 
     for (src = dst = 0; src < len; src++)
-    {
-        chtype ch = srcp[src];
-        if (ch == DUMMY_CHAR_NEXT_TO_FULLWIDTH)
-            continue;
+        if( (srcp[src] & A_CHARTEXT) != DUMMY_CHAR_NEXT_TO_FULLWIDTH)
+        {
+            const chtype ch = srcp[src];
+            chtype char_out = ch & A_CHARTEXT;
 
 #if defined( CHTYPE_LONG) && (CHTYPE_LONG >= 2)
-        ci[dst].Attributes = pdc_atrtab[((ch >> PDC_ATTR_SHIFT) & 0x1f)
+            ci[dst].Attributes = pdc_atrtab[((ch >> PDC_ATTR_SHIFT) & 0x1f)
                      | (((ch >> PDC_COLOR_SHIFT) & 0xff) << 5)];
 #else
-        ci[dst].Attributes = pdc_atrtab[ch >> PDC_ATTR_SHIFT];
+            ci[dst].Attributes = pdc_atrtab[ch >> PDC_ATTR_SHIFT];
 #endif
 
 #ifdef CHTYPE_LONG
-        if (ch & A_ALTCHARSET && !(ch & 0xff80))
-            ch = acs_map[ch & 0x7f];
+            if (ch & A_ALTCHARSET && !(ch & 0xff80))
+                char_out = acs_map[ch & 0x7f];
+            if( char_out > MAX_UNICODE)   /* combining chars: not */
+                char_out = ' ';           /* supported yet */
+            else if( char_out > 0xffff)  /* SMP chars: use surrogates */
+            {
+                ci[dst].Char.UnicodeChar = (WCHAR)( 0xd800 | (char_out>>10));
+                ci[dst + 1] = ci[dst];
+                dst++;
+                char_out = (chtype)( 0xdc00 | (char_out & 0x3ff));
+            }
 #endif
-        ci[dst].Char.UnicodeChar = (WCHAR)( ch & A_CHARTEXT);
+            ci[dst].Char.UnicodeChar = (WCHAR)char_out;
 
-        dst++;
-    }
+            dst++;
+        }
 
     bufSize.X = dst;
     bufSize.Y = 1;

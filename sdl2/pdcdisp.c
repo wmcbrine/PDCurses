43d5bc8fc16f049949f1430f14127e6e19776580
William McBrine <wmcbrine@gmail.com>
2018-12-04 13:35:03 -0500

    The ACS tables are somewhat redundant, so break them out into /common.

--- sdl2/pdcdisp.c
+++ sdl2/pdcdisp.c
@@ -5,57 +5,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-chtype acs_map[128] =
-{
-    PDC_ACS(0), PDC_ACS(1), PDC_ACS(2), PDC_ACS(3), PDC_ACS(4),
-    PDC_ACS(5), PDC_ACS(6), PDC_ACS(7), PDC_ACS(8), PDC_ACS(9),
-    PDC_ACS(10), PDC_ACS(11), PDC_ACS(12), PDC_ACS(13), PDC_ACS(14),
-    PDC_ACS(15), PDC_ACS(16), PDC_ACS(17), PDC_ACS(18), PDC_ACS(19),
-    PDC_ACS(20), PDC_ACS(21), PDC_ACS(22), PDC_ACS(23), PDC_ACS(24),
-    PDC_ACS(25), PDC_ACS(26), PDC_ACS(27), PDC_ACS(28), PDC_ACS(29),
-    PDC_ACS(30), PDC_ACS(31), ' ', '!', '"', '#', '$', '%', '&', '\'',
-    '(', ')', '*',
-
 #ifdef PDC_WIDE
-    0x2192, 0x2190, 0x2191, 0x2193,
+# include "../common/acsgr.h"
 #else
-    PDC_ACS(0x1a), PDC_ACS(0x1b), PDC_ACS(0x18), PDC_ACS(0x19),
+# include "../common/acs437.h"
 #endif
-
-    '/',
-
-#ifdef PDC_WIDE
-    ACS_BLOCK,
-#else
-    0xdb,
-#endif
-
-    '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=',
-    '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-    'X', 'Y', 'Z', '[', '\\', ']', '^', '_',
-
-#ifdef PDC_WIDE
-    0x2666, 0x2592,
-#else
-    PDC_ACS(0x04), 0xb1,
-#endif
-
-    'b', 'c', 'd', 'e',
-
-#ifdef PDC_WIDE
-    0x00b0, 0x00b1, 0x2591, 0x00a4, ACS_LRCORNER, ACS_URCORNER,
-    ACS_ULCORNER, ACS_LLCORNER, ACS_PLUS, ACS_S1, ACS_S3, ACS_HLINE,
-    ACS_S7, ACS_S9, ACS_LTEE, ACS_RTEE, ACS_BTEE, ACS_TTEE, ACS_VLINE,
-    0x2264, 0x2265, 0x03c0, 0x2260, 0x00a3, 0x00b7,
-#else
-    0xf8, 0xf1, 0xb0, PDC_ACS(0x0f), 0xd9, 0xbf, 0xda, 0xc0, 0xc5, 0x2d,
-    0x2d, 0xc4, 0x2d, 0x5f, 0xc3, 0xb4, 0xc1, 0xc2, 0xb3, 0xf3, 0xf2,
-    0xe3, 0xd8, 0x9c, 0xf9,
-#endif
-
-    PDC_ACS(127)
-};
 
 Uint32 pdc_lastupdate = 0;
 
--- wincon/pdcdisp.c
+++ wincon/pdcdisp.c
@@ -5,57 +5,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-chtype acs_map[128] =
-{
-    PDC_ACS(0), PDC_ACS(1), PDC_ACS(2), PDC_ACS(3), PDC_ACS(4),
-    PDC_ACS(5), PDC_ACS(6), PDC_ACS(7), PDC_ACS(8), PDC_ACS(9),
-    PDC_ACS(10), PDC_ACS(11), PDC_ACS(12), PDC_ACS(13), PDC_ACS(14),
-    PDC_ACS(15), PDC_ACS(16), PDC_ACS(17), PDC_ACS(18), PDC_ACS(19),
-    PDC_ACS(20), PDC_ACS(21), PDC_ACS(22), PDC_ACS(23), PDC_ACS(24),
-    PDC_ACS(25), PDC_ACS(26), PDC_ACS(27), PDC_ACS(28), PDC_ACS(29),
-    PDC_ACS(30), PDC_ACS(31), ' ', '!', '"', '#', '$', '%', '&', '\'',
-    '(', ')', '*',
-
 #ifdef PDC_WIDE
-    0x2192, 0x2190, 0x2191, 0x2193,
+# include "../common/acsuni.h"
 #else
-    PDC_ACS(0x1a), PDC_ACS(0x1b), PDC_ACS(0x18), PDC_ACS(0x19),
+# include "../common/acs437.h"
 #endif
-
-    '/',
-
-#ifdef PDC_WIDE
-    0x2588,
-#else
-    0xdb,
-#endif
-
-    '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=',
-    '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-    'X', 'Y', 'Z', '[', '\\', ']', '^', '_',
-
-#ifdef PDC_WIDE
-    0x2666, 0x2592,
-#else
-    PDC_ACS(0x04), 0xb1,
-#endif
-
-    'b', 'c', 'd', 'e',
-
-#ifdef PDC_WIDE
-    0x00b0, 0x00b1, 0x2591, 0x00a4, 0x2518, 0x2510, 0x250c, 0x2514,
-    0x253c, 0x23ba, 0x23bb, 0x2500, 0x23bc, 0x23bd, 0x251c, 0x2524,
-    0x2534, 0x252c, 0x2502, 0x2264, 0x2265, 0x03c0, 0x2260, 0x00a3,
-    0x00b7,
-#else
-    0xf8, 0xf1, 0xb0, PDC_ACS(0x0f), 0xd9, 0xbf, 0xda, 0xc0, 0xc5, 0x2d,
-    0x2d, 0xc4, 0x2d, 0x5f, 0xc3, 0xb4, 0xc1, 0xc2, 0xb3, 0xf3, 0xf2,
-    0xe3, 0xd8, 0x9c, 0xf9,
-#endif
-
-    PDC_ACS(127)
-};
 
 DWORD pdc_last_blink;
 static bool blinked_off = FALSE;

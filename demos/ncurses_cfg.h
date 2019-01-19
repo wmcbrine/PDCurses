5abce67c1ae96c5146dae89b188ff2e866b43465
William McBrine <wmcbrine@gmail.com>
2019-01-18 19:23:47 -0500

    Add a few more ncurses_tests; update to 6.1.

--- demos/ncurses_cfg.h
+++ demos/ncurses_cfg.h
@@ -1,6 +1,6 @@
 /* This file is only used with the ncurses test programs.
  *
- * Have ncurses-5.9 unpacked in your $(HOME) (you don't need to build
+ * Have ncurses-6.1 unpacked in your $(HOME) (you don't need to build
  * it), or edit ncurses_testdir appropriately in the Makefile or
  * nctests.mif. Configure and build PDCurses, and:
  *
@@ -10,9 +10,11 @@
  * Builds: bs gdc hanoi knight tclock ncurses
  */
 
-#define NCURSES_MOUSE_VERSION 2
+#define PDC_NCMOUSE
+#define DATA_DIR "."
 #define NCURSES_CONST
 #define NCURSES_EXPORT(foo) foo
+#define popup_msg(bar, baz)
 
 #define RETSIGTYPE void
 #define TIME_WITH_SYS_TIME 1
@@ -25,6 +27,7 @@
 
 #define ExitProgram exit
 
+#define HAVE_ASSUME_DEFAULT_COLORS 1
 #define HAVE_CURSES_VERSION 1
 #define HAVE_GETBEGX 1
 #define HAVE_GETCURX 1
@@ -32,14 +35,17 @@
 #define HAVE_GETNSTR 1
 #define HAVE_GETTIMEOFDAY 1
 #define HAVE_GETWIN 1
+#define HAVE_HALFDELAY 1
 #define HAVE_LIBPANEL 1
 #define HAVE_LOCALE_H 1
 #define HAVE_MATH_H 1
 #define HAVE_NAPMS 1
+#define HAVE_NEWPAD 1
 #define HAVE_PANEL_H 1
 #define HAVE_PUTWIN 1
 #define HAVE_SLK_COLOR 1
 #define HAVE_SLK_INIT 1
+#define HAVE_USE_DEFAULT_COLORS 1
 #define HAVE_WRESIZE 1
 
 #ifdef PDC_WIDE

225f964edb0e8c67cbe811aeeb2903b4c0be5e54
William McBrine <wmcbrine@gmail.com>
2019-03-31 20:41:38 -0400

    Python is overkill for this.

--- man/mkman.sh
+++ man/mkman.sh
@@ -1,7 +1,7 @@
 echo Definitions and Variables \(curses.h\) > MANUAL.md
 echo ==================================== >> MANUAL.md
-./manext.py ../curses.h >> MANUAL.md
+./manext.awk ../curses.h >> MANUAL.md
 echo Functions >> MANUAL.md
 echo ========= >> MANUAL.md
-./manext.py ../pdcurses/*.c >> MANUAL.md
-./manext.py ../x11/*.c >> MANUAL.md
+./manext.awk ../pdcurses/*.c >> MANUAL.md
+./manext.awk ../x11/*.c >> MANUAL.md

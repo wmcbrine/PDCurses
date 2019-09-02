3a2b906a25ff37689da0620afeb1980439309220
William McBrine <wmcbrine@gmail.com>
2019-09-02 11:25:14 -0400

    Re-ordered to match pdcscrn.c; hopefully more sensible.

--- x11/pdcx11.h
+++ x11/pdcx11.h
@@ -20,6 +20,9 @@
 
 #include <Xatom.h>
 
+#define XCURSESDISPLAY (XtDisplay(drawing))
+#define XCURSESWIN     (XtWindow(drawing))
+
 typedef struct
 {
     int lines;
@@ -56,7 +59,24 @@ typedef struct
     int textBlinkRate;
 } XCursesAppData;
 
+extern Pixel colors[PDC_MAXCOL];
+
 extern XCursesAppData xc_app_data;
+extern XtAppContext app_context;
+extern Widget topLevel, drawing;
+
+extern GC normal_gc, rect_cursor_gc, italic_gc, bold_gc;
+extern int font_height, font_width, font_ascent, font_descent;
+extern int window_width, window_height;
+extern bool blinked_off;
+
+extern int resize_window_width, resize_window_height;
+extern bool window_entered;
+extern bool xc_resize_now;
+
+extern XIC Xic;
+extern bool vertical_cursor;
+extern bool visible_cursor;
 
 int PDC_display_cursor(int, int, int, int, int);
 
@@ -65,23 +85,3 @@ void XC_blink_text(XtPointer, XtIntervalId *);
 int XC_kb_setup(void);
 void XC_redraw_cursor(void);
 bool XC_scrollbar_init(const char *);
-
-extern XtAppContext app_context;
-extern Widget topLevel, drawing;
-
-#define XCURSESDISPLAY (XtDisplay(drawing))
-#define XCURSESWIN     (XtWindow(drawing))
-
-extern Pixel colors[PDC_MAXCOL];
-
-extern GC normal_gc, rect_cursor_gc, italic_gc, bold_gc;
-extern int font_height, font_width, font_ascent, font_descent;
-extern int window_width, window_height;
-extern int resize_window_width, resize_window_height;
-
-extern bool xc_resize_now;
-extern XIC Xic;
-extern bool blinked_off;
-extern bool vertical_cursor;
-extern bool visible_cursor;
-extern bool window_entered;

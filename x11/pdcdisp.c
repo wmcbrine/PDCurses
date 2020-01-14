cc1389347433f6b8a85a219bc3a9bf4d4dd0ce00
Bill-Gray <pluto@projectpluto.com>
2020-01-13 21:06:06 -0500

    Now uses the 'common' ACS defs.

--- x11/pdcdisp.c
+++ x11/pdcdisp.c
@@ -4,44 +4,15 @@
 
 #include <string.h>
 
+#ifdef CHTYPE_LONG
+
 #ifdef PDC_WIDE
-# include "../common/acsuni.h"
+   #define USE_UNICODE_ACS_CHARS 1
 #else
+   #define USE_ISO8859_CHARSET
+#endif
 
-chtype acs_map[128] =
-{
-    PDC_ACS(0), PDC_ACS(1), PDC_ACS(2), PDC_ACS(3), PDC_ACS(4),
-    PDC_ACS(5), PDC_ACS(6), PDC_ACS(7), PDC_ACS(8), PDC_ACS(9),
-    PDC_ACS(10), PDC_ACS(11), PDC_ACS(12), PDC_ACS(13), PDC_ACS(14),
-    PDC_ACS(15), PDC_ACS(16), PDC_ACS(17), PDC_ACS(18), PDC_ACS(19),
-    PDC_ACS(20), PDC_ACS(21), PDC_ACS(22), PDC_ACS(23), PDC_ACS(24),
-    PDC_ACS(25), PDC_ACS(26), PDC_ACS(27), PDC_ACS(28), PDC_ACS(29),
-    PDC_ACS(30), PDC_ACS(31), ' ', '!', '"', '#', '$', '%', '&', '\'',
-    '(', ')', '*',
-
-    '>', '<', '^', 'v',
-
-    '/',
-
-    PDC_ACS(0),
-
-    '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=',
-    '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-    'X', 'Y', 'Z', '[', '\\', ']', '^', '_',
-
-    PDC_ACS(1), PDC_ACS(2),
-
-    'b', 'c', 'd', 'e',
-
-    PDC_ACS(7), PDC_ACS(8), '#', 0xa4, PDC_ACS(11), PDC_ACS(12),
-    PDC_ACS(13), PDC_ACS(14), PDC_ACS(15), PDC_ACS(16), PDC_ACS(17),
-    PDC_ACS(18), PDC_ACS(19), PDC_ACS(20), PDC_ACS(21), PDC_ACS(22),
-    PDC_ACS(23), PDC_ACS(24), PDC_ACS(25), PDC_ACS(26), PDC_ACS(27),
-    PDC_ACS(28), PDC_ACS(29), PDC_ACS(30), 0xb7,
-
-    PDC_ACS(127)
-};
+#include "../common/acs_defs.h"
 
 #endif
 

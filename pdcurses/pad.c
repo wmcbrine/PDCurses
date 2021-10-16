6db050244c4b25294c0835c41ddc1daded8c4024
GiorgosXou <gxousos@gmail.com>
2021-10-16 11:26:06 +0300

    Fixed: access violation reading
    int sline = sy1; int pline = py;

--- pdcurses/pad.c
+++ pdcurses/pad.c
@@ -178,14 +178,7 @@ int prefresh(WINDOW *win, int py, int px, int sy1, int sx1, int sy2, int sx2)
 
 int pnoutrefresh(WINDOW *w, int py, int px, int sy1, int sx1, int sy2, int sx2)
 {
-    int num_cols;
-    int sline = sy1;
-    int pline = py;
-
     PDC_LOG(("pnoutrefresh() - called\n"));
-
-    if (!w || !(w->_flags & (_PAD|_SUBPAD)) || (sy2 >= LINES) || (sx2 >= COLS))
-        return ERR;
 
     if (py < 0)
         py = 0;
@@ -196,9 +189,16 @@ int pnoutrefresh(WINDOW *w, int py, int px, int sy1, int sx1, int sy2, int sx2)
     if (sx1 < 0)
         sx1 = 0;
 
+    if (!w || !(w->_flags & (_PAD|_SUBPAD)) || (sy2 >= LINES) || (sx2 >= COLS))
+        return ERR;
+    
     if (sy2 < sy1 || sx2 < sx1)
         return ERR;
 
+    int num_cols;
+    int sline = sy1;
+    int pline = py;
+    
     num_cols = min((sx2 - sx1 + 1), (w->_maxx - px));
 
     while (sline <= sy2)

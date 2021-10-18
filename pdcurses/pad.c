2f31a22d66575049082d647cf3f9495cf8fd76f4
William McBrine <wmcbrine@gmail.com>
2021-10-17 20:11:05 -0400

    Extra spaces

--- pdcurses/pad.c
+++ pdcurses/pad.c
@@ -181,7 +181,7 @@ int pnoutrefresh(WINDOW *w, int py, int px, int sy1, int sx1, int sy2, int sx2)
     int num_cols;
     int sline;
     int pline;
-    
+
     PDC_LOG(("pnoutrefresh() - called\n"));
 
     if (py < 0)
@@ -193,14 +193,14 @@ int pnoutrefresh(WINDOW *w, int py, int px, int sy1, int sx1, int sy2, int sx2)
     if (sx1 < 0)
         sx1 = 0;
 
-    if ((!w || !(w->_flags & (_PAD|_SUBPAD)) || 
+    if ((!w || !(w->_flags & (_PAD|_SUBPAD)) ||
         (sy2 >= LINES) || (sx2 >= COLS)) ||
         (sy2 < sy1) || (sx2 < sx1))
         return ERR;
 
     sline = sy1;
     pline = py;
-    
+
     num_cols = min((sx2 - sx1 + 1), (w->_maxx - px));
 
     while (sline <= sy2)

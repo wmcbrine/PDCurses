67a1f4c50515a443e30e2fe6e74ad11e06a60346
waltje <decwiz@yahoo.com>
2023-10-08 16:58:06 -0400

    Remove old files.

--- demos/mk.cmd
+++
@@ -1,1 +0,0 @@
-cl -I.. -DPDC_DLL_BUILD -DPDC_WIDE testcurs.c ..\..\lib\%ARCH%\pdcurses.lib 
--- wincon/mk.cmd
+++
@@ -1,8 +0,0 @@
-@nmake -fmakefile.vc WIDE=Y UTF8=Y DLL=Y 
-@move *.dll ..\..\bin\%ARCH%
-@move *.lib ..\..\lib\%ARCH%
-@del *.dll *.lib *.exp *.res *.obj
-
-@nmake -fmakefile.vc WIDE=Y UTF8=Y
-@move *.lib ..\..\lib\%ARCH%\pdcurses_s.lib
-@del *.dll *.lib *.exp *.res *.obj

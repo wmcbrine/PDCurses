e7115da24fbcd91b23676476599b0db581931a91
scivision <scivision@users.noreply.github.com>
2024-03-21 17:18:40 -0400

    x11: remove unneeded extra includes

--- cmake/x11.cmake
+++ cmake/x11.cmake
@@ -8,12 +8,10 @@ if(PKG_CONFIG_FOUND)
 
 pkg_check_modules(_xt xt)
 pkg_check_modules(_x11 x11)
-pkg_check_modules(_xmu xmu)
-pkg_check_modules(_xpm xpm)
-pkg_check_modules(_xaw REQUIRED xaw7)
 
 endif()
 
+# Ubuntu may need this
 find_path(_xt_inc
 NAMES Intrinsic.h
 HINTS ${_xt_INCLUDE_DIRS} ${X11_INCLUDE_DIR}
@@ -21,6 +19,7 @@ PATH_SUFFIXES X11
 REQUIRED
 )
 
+# macOS may need this
 find_path(_xatom_inc
 NAMES Xatom.h
 HINTS ${_x11_INCLUDE_DIRS} ${X11_INCLUDE_DIR}
@@ -28,32 +27,8 @@ PATH_SUFFIXES X11
 REQUIRED
 )
 
-find_path(_xmu_inc
-NAMES StdSel.h
-HINTS ${_xmu_INCLUDE_DIRS} ${X11_INCLUDE_DIR}
-PATH_SUFFIXES X11/Xmu
-REQUIRED
-)
-
-find_path(_xpm_inc
-NAMES xpm.h
-HINTS ${_xpm_INCLUDE_DIRS} ${X11_INCLUDE_DIR}
-PATH_SUFFIXES X11
-REQUIRED
-)
-
-find_path(_xaw_inc
-NAMES Box.h
-HINTS ${_xaw_INCLUDE_DIRS} ${X11_INCLUDE_DIR}
-PATH_SUFFIXES X11/Xaw
-REQUIRED
-)
-
 list(APPEND X11_INCLUDE_DIR
 ${_xt_inc} ${_xatom_inc}
-${_xmu_inc} ${_xmu_inc}/..
-${_xpm_inc}
-${_xaw_inc} ${_xaw_inc}/..
 )
 
 message(STATUS "X11_INCLUDE_DIR: ${X11_INCLUDE_DIR}

3307af0c131a35b23813390a825fb05673885a8c
Michael Hirsch <scivision@users.noreply.github.com>
2024-03-21 18:25:32 -0400

    scope compile options

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,15 +1,3 @@
-include(CheckSymbolExists)
-
-check_symbol_exists("vsnprintf" "stdio.h" HAVE_VSNPRINTF)
-check_symbol_exists("vsscanf" "stdio.h" HAVE_VSSCANF)
-
-add_compile_definitions(
-$<$<BOOL:${HAVE_VSNPRINTF}>:HAVE_VSNPRINTF>
-$<$<BOOL:${HAVE_VSSCANF}>:HAVE_VSSCANF>
-)
-
-# --- compiler options
-
 if(CMAKE_C_COMPILER_ID STREQUAL OpenWatcom)
   message(STATUS "OpenWatcom: $ENV{WATCOM}
   Host: ${CMAKE_HOST_SYSTEM_NAME} ${CMAKE_HOST_SYSTEM_VERSION}
@@ -17,6 +5,6 @@ if(CMAKE_C_COMPILER_ID STREQUAL OpenWatcom)
   ")
   # https://wiki.archlinux.org/title/Open_Watcom
   add_compile_options(-bt=dos -bcl=dos)
-else()
+elseif(NOT MSVC)
   add_compile_options(-Wall)
 endif()

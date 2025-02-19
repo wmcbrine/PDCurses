9974339e818efab405c5053dc8763fab59858f73
William McBrine <wmcbrine@gmail.com>
2025-02-18 22:57:30 -0500

    Moar tables

--- curses.h
+++ curses.h
@@ -9,26 +9,28 @@
 
 Define before inclusion (only those needed):
 
-    XCURSES         if building / built for X11
-    PDC_RGB         if you want to use RGB color definitions
-                    (Red = 1, Green = 2, Blue = 4) instead of BGR
-    PDC_WIDE        if building / built with wide-character support
-    PDC_DLL_BUILD   if building / built as a Windows DLL
-    PDC_NCMOUSE     to use the ncurses mouse API instead
-                    of PDCurses' traditional mouse API
+   Macro         | Meaning / value
+   :-------------|-------------------------------------------------
+   XCURSES       | if building / built for X11
+   PDC_RGB       | RGB color (Red = 1, Green = 2, Blue = 4) vs. BGR
+   PDC_WIDE      | if building / built with wide-character support
+   PDC_DLL_BUILD | if building / built as a Windows DLL
+   PDC_NCMOUSE   | use ncurses mouse API vs. traditional PDCurses
 
 Defined by this header:
 
-    PDCURSES        PDCurses-only features are available
-    PDC_BUILD       API build version
-    PDC_VER_MAJOR   major version number
-    PDC_VER_MINOR   minor version number
-    PDC_VERDOT      version string
+   Macro         | Meaning / value
+   :-------------|-------------------------------------------------
+   PDCURSES      | PDCurses-only features are available
+   PDC_BUILD     | API build version
+   PDC_VER_MAJOR | major version number
+   PDC_VER_MINOR | minor version number
+   PDC_VERDOT    | version string
 
 **man-end****************************************************************/
 
 #define PDCURSES        1
-#define PDC_BUILD    3907
+#define PDC_BUILD    3908
 #define PDC_VER_MAJOR   3
 #define PDC_VER_MINOR   9
 #define PDC_VERDOT   "3.9"

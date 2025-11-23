09cf16db29753305e4241d4ae609aac997fa730d
William McBrine <wmcbrine@gmail.com>
2025-11-23 01:16:49 -0500

    Checking for WT_SESSION is unreliable; alternate method suggested by Vincent Fatica at https://github.com/microsoft/terminal/issues/7434 . To Do: This may affect ConEmu. Also, modern conhost supports italic.

--- wincon/pdcscrn.c
+++ wincon/pdcscrn.c
@@ -401,7 +401,7 @@ int PDC_scr_open(void)
 
     is_nt = !(GetVersion() & 0x80000000);
 
-    pdc_wt = !!getenv("WT_SESSION");
+    pdc_wt = !((BOOL) SendMessage(GetConsoleWindow(), WM_GETICON, 0, 0));
     str = pdc_wt ? NULL : getenv("ConEmuANSI");
     pdc_conemu = !!str;
     pdc_ansi = pdc_wt ? TRUE : pdc_conemu ? !strcmp(str, "ON") : FALSE;
